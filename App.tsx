6d2eb4c57e606dd07bb1b1c04aec31cf66f13938
Leandro Iglezias <leandro.iglezias@lnaix.com.br>
2025-03-15 20:23:21 -0300

    Add feature on and off light

--- App.tsx
+++ App.tsx
@@ -1,19 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+
+import symbolOn from './assets/pictures/symbol-on.png';
+import symbolOff from './assets/pictures/symbol-off.png';
 
 export default function App() {
+  const [isActive, setIsActive] = useState(false);
+
+  function handleSymbol (){
+    setIsActive((oldValue:boolean) => {
+        return !oldValue;
+    })
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={isActive ? styles.containerOn : styles.containerOff}>
+      <TouchableOpacity onPress={handleSymbol}>
+          <Image source={isActive ? symbolOn : symbolOff}/>
+      </TouchableOpacity>
+      {/**<Image source={symbolOff}></Image>*/}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerOn: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerOff: {
+    flex: 1,
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
